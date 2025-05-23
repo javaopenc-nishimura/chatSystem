e64572266f2e16c6b38d72839b93d2b73e3526d4
nishimura <you@example.com>
2025-05-23 13:18:39 +0900

    タスク６までの編集内容

--- src/main/java/com/example/demo/form/ChatController.java
+++ src/main/java/com/example/demo/form/ChatController.java
@@ -1,5 +1,47 @@
 package com.example.demo.form;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dao.ChatDao;
+import com.example.demo.entity.EntForm;
+
+@Controller
 public class ChatController {
+	private final ChatDao chatdao;
+	public ChatController(ChatDao chatdao) {
+	this.chatdao = chatdao;
+	}
+	
+	@RequestMapping("")
+	public String home(Model model) {
+		model.addAttribute("message","chatSystemへようこそ");
+		return "index";
+	}
+	
+	@RequestMapping("/view")
+	public String view(Model model) {
+		model.addAttribute("title","チャットルーム");
+		return "chat/view";
+	}
+	
+	@RequestMapping("/view1")
+	public String complete(Model model, Form form) {
+		model.addAttribute("title","チャットルーム");
+	//データベース処理
+	EntForm entform = new EntForm();
+	entform.setName(form.getName1());
+	entform.setComment(form.getComment());
+	chatdao.insertDb(entform);
+	
+	//一覧表示
+	List<EntForm> list = chatdao.searchDb();
+	model.addAttribute("dbList",list);
+	
+	return "chat/view";
+	}
 
 }
---
+++ src/main/java/com/example/demo/form/Form.java
@@ -0,0 +1,20 @@
+package com.example.demo.form;
+
+public class Form {
+	private String name1;
+	private String comment;
+	
+	public String getName1() {
+		return name1;
+	}
+	public void setName1(String name1) {
+		this.name1 = name1;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+}
